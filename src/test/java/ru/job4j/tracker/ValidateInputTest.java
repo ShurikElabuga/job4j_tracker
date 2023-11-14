4a05594459bd2a5d239acbec1acf7c463c7505fd
Aleksandr <ShurikElabuga@yandex.ru>
2023-11-14 13:10:31 +0300

    2.1. Тест на ValidateInput [#311887 #459637]

--- src/test/java/ru/job4j/tracker/ValidateInputTest.java
+++ src/test/java/ru/job4j/tracker/ValidateInputTest.java
@@ -33,18 +33,18 @@ class ValidateInputTest {
     @Test
     void whenSeveralValidInput() {
         Output out = new StubOutput();
-        Input in1 = new MockInput(new String[] {"0"});
-        Input in2 = new MockInput(new String[] {"1"});
-        Input in3 = new MockInput(new String[] {"2"});
-        ValidateInput input1 = new ValidateInput(out, in1);
-        int selected1 = input1.askInt("Enter menu:");
-        ValidateInput input2 = new ValidateInput(out, in2);
-        int selected2 = input2.askInt("Enter menu:");
-        ValidateInput input3 = new ValidateInput(out, in3);
-        int selected3 = input3.askInt("Enter menu:");
-        assertThat(selected1).isEqualTo(0);
-        assertThat(selected2).isEqualTo(1);
-        assertThat(selected3).isEqualTo(2);
+        Input in = new MockInput(new String[] {"0"});
+        ValidateInput input = new ValidateInput(out, in);
+        int selected = input.askInt("Enter menu:");
+        assertThat(selected).isEqualTo(0);
+        in = new MockInput(new String[] {"1"});
+        input = new ValidateInput(out, in);
+        selected = input.askInt("Enter menu:");
+        assertThat(selected).isEqualTo(1);
+        in = new MockInput(new String[] {"2"});
+        input = new ValidateInput(out, in);
+        selected = input.askInt("Enter menu:");
+        assertThat(selected).isEqualTo(2);
     }
 
     @Test
