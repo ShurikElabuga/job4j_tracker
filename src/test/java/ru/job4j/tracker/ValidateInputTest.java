8ea97ed7f82ac465d0004eee7a95797c5b082777
Aleksandr <ShurikElabuga@yandex.ru>
2023-11-16 00:06:15 +0300

    2.1. Тест на ValidateInput [#311887 #459637]

--- src/test/java/ru/job4j/tracker/ValidateInputTest.java
+++ src/test/java/ru/job4j/tracker/ValidateInputTest.java
@@ -33,18 +33,15 @@ class ValidateInputTest {
     @Test
     void whenSeveralValidInput() {
         Output out = new StubOutput();
-        Input in = new MockInput(new String[] {"0"});
+        Input in = new MockInput(new String[] {"0", "1", "2"});
         ValidateInput input = new ValidateInput(out, in);
-        int selected = input.askInt("Enter menu:");
-        assertThat(selected).isEqualTo(0);
-        in = new MockInput(new String[] {"1"});
-        input = new ValidateInput(out, in);
-        selected = input.askInt("Enter menu:");
-        assertThat(selected).isEqualTo(1);
-        in = new MockInput(new String[] {"2"});
-        input = new ValidateInput(out, in);
-        selected = input.askInt("Enter menu:");
-        assertThat(selected).isEqualTo(2);
+        int[] selected = new int[3];
+        selected[0] = input.askInt("Enter menu:");
+        assertThat(Arrays.equals(selected, new int[] {0}));
+        selected[1] = input.askInt("Enter menu:");
+        assertThat(Arrays.equals(selected, new int[] {1}));
+        selected[2] = input.askInt("Enter menu:");
+        assertThat(Arrays.equals(selected, new int[] {2}));
     }
 
     @Test
