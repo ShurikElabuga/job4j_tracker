097f4aca3ab717903b2b08ca382cabe62d46dc9f
Aleksandr <ShurikElabuga@yandex.ru>
2024-02-20 22:31:26 +0300

    4. Сортировка номера [#221284]

---
+++ src/main/java/ru/job4j/collection/LexSort.java
@@ -0,0 +1,13 @@
+package ru.job4j.collection;
+
+import java.util.Comparator;
+
+public class LexSort implements Comparator<String> {
+    @Override
+    public int compare(String o1, String o2) {
+        String[] str1 = o1.split("\\.");
+        String[] str2 = o2.split("\\.");
+
+        return Integer.compare(Integer.parseInt(str1[0]), Integer.parseInt(str2[0]));
+    }
+}
